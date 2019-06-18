30317ebc7133ea1a3344589d25258a9212be3566
Rapando <samson.rapando@betika.com>
2019-06-18 17:07:45 +0300

    updated version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='rcurl',
-    version='0.0.1',
+    version='0.0.2',
     description="Generate curl commands from python-requests",
     url="https://github.com/rapando/rcurl",
     author="Samson Rapando",
